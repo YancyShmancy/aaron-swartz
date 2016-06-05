af8f8d4cc7e16eecc7d370068fe8b68da050edaa
Zoey Yancy <yancyshmancy@gmail.com>
2016-06-05 00:03:34 -0400

    Started adding assets

--- .tmp/assets/js/app/main.js
+++ .tmp/assets/js/app/main.js
@@ -1,24 +1,42 @@
 var scrollTop;
 var pageHeight; 
 
+$.fn.parallax = function ( resistance, mouse ) {
+	$el = $(this);
+	TweenLite.set( $el, {
+		x : -(( mouse.clientX - (window.innerWidth/2) ) / resistance ),
+//		y : -(( mouse.clientY - (window.innerHeight/4) ) / resistance )
+	});
+};
+
 var tl = new TimelineMax();
 
-tl.to({}, 10, {})
-    .to('.header-container>h1', 1, {opacity: 1}, '-=10' )
-    .to('.header-container>h2', 1, {opacity: 1}, '-=9')
-    .to('#home', 2, {top: '-100%'}, '-=7')
-    .to('#accomp', 2, {top: 0}, '-=7')
+tl.to({}, 20, {})
+    .to('.intro', 3, {opacity: 1}, '-=20')
+    .to('.intro-finish', 1, {opacity: 1}, '-=17')
+    .to('.intro', 1, {opacity: 0}, '-=17')
+    .to('.intro-finish', 1, {opacity: 0}, '-=15')
+    .to('.title-container>h1', 1, {opacity: 1}, '-=14' )
+    .to('.title-container>h2', 1, {opacity: 1}, '-=13')
+    .to('#home', 3, {top: '-100%'}, '-=10')
+    .to('#accomp', 3, {top: 0}, '-=10')
     .to('#home', 3, {background: 'darkgreen'}, '-=4');
 
 tl.stop();
+
+$(document).mousemove(function(e){
+    $('#home-aaron').parallax(250, e);
+    $('#accomp-aaron').parallax(50, e);
+});
     
 
 $(window).on("scroll", function() {
     pageHeight = $('body').height();
     scrollTop = $(window).scrollTop();
-    
-    console.log(pageHeight);
-    tl.seek( scrollTop / pageHeight *  10);
-    console.log( scrollTop/ pageHeight * 10);
+    tl.seek( scrollTop / pageHeight *  20);
 });
 
+$('.nav-icon').on('click', function(){
+    $('#nav').toggleClass('active');
+    $(this).toggleClass('open');
+})
